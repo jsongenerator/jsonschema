a4329c6142f55be1df6d173ff3b19a00a9a1e74b
jsongenerator <fromosom@gmail.com>
2025-04-04 11:39:37 +0200

    application.js

---
+++ application.js
@@ -0,0 +1,34 @@
+// Minimalistyczny uniwersalny loader
+(function() {
+    // --- Automatycznie wczytaj CSS ---
+    var cssLink = 'https://twoj-link-do-css.example/style.css'; // <<< podmień na swój link
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssLink;
+    document.head.appendChild(link);
+
+    // --- Prosty zestaw narzędzi do manipulacji DOM ---
+    window.domTools = {
+        setText: function(selector, text) {
+            var el = document.querySelector(selector);
+            if (el) el.textContent = text;
+        },
+        setHTML: function(selector, html) {
+            var el = document.querySelector(selector);
+            if (el) el.innerHTML = html;
+        },
+        addClass: function(selector, className) {
+            var el = document.querySelector(selector);
+            if (el) el.classList.add(className);
+        },
+        removeClass: function(selector, className) {
+            var el = document.querySelector(selector);
+            if (el) el.classList.remove(className);
+        },
+        setAttr: function(selector, attr, value) {
+            var el = document.querySelector(selector);
+            if (el) el.setAttribute(attr, value);
+        }
+    };
+
+})();
